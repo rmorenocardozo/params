66ebe6a5ef13b48be48ead6bbdbac0ac2bd734de
Ricardo <r.morenocardozo@gmail.com>
2019-06-13 20:01:46 -0500

    Params 2

--- solution.rb
+++ solution.rb
@@ -1,9 +1,5 @@
 require 'sinatra'
 
-get '/' do
-  if params[:nombre] == "Juan"
-    "<h1>Hola #{params[:nombre]}!</h1>"
-  else
-    "<h1>Hola desconocido!</h1>"
-  end
+get '/:nombre' do
+    "<h1>Hola #{params[:nombre].capitalize}!</h1>"
 end
